bbb6430f77d82a5b6e709b2109d19a7be8f5b977
pencilCool <yhtang_xt@163.com>
2017-04-13 16:38:41 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by tangyuhua on 2017/4/13.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by tangyuhua on 2017/4/13.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
